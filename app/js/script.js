85e33066a13403b469792ba4754fa5db13787c03
ibrahimtrahim <trahimbrahim@gmail.com>
2023-08-25 16:25:33 +0000

    New Project

---
+++ app/js/script.js
@@ -0,0 +1,19 @@
+let menubtn = document.getElementById("menubtn");
+let overlay = document.getElementById("overlay");
+let menu = document.getElementById("menu");
+
+overlay.style.opacity = "0";
+overlay.style.visibility = "hidden";
+
+menubtn.onclick = function(){
+    if(overlay.style.opacity == "0"){
+        overlay.style.opacity = "1";
+        overlay.style.visibility = "visible";
+        menu.src = "images/icon-close.svg";
+    }
+    else {
+        overlay.style.opacity = "0";
+        overlay.style.visibility = "hidden";
+        menu.src = "images/icon-hamburger.svg";
+    }
+}
